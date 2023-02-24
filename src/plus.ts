6b65c3fcda9671d840c1d7919f54a7c2ce708881
Shigma <shigma10826@gmail.com>
2023-02-24 23:10:43 +0800

    length restriction

--- src/plus.ts
+++ src/plus.ts
@@ -3,21 +3,20 @@ import { ToBinary, ToDecimal } from './binary'
 type Result = [[[0, 1], [1, 0]], [[1, 0], [0, 1]]]
 type Carry = [[[0, 0], [0, 1]], [[0, 1], [1, 1]]]
 
-type HalfAdd<B extends number[], C extends number> = 
-  | B extends [number, ...infer X extends number[]]
-  ? [Result[0][B[0]][C], ...HalfAdd<X, Carry[0][B[0]][C]>]
+type HalfAdd<B extends number[], C extends number, R extends number[] = []> = 
+  | R['length'] extends 32
+  ? R
+  : B extends [number, ...infer X extends number[]]
+  ? HalfAdd<X, Carry[0][B[0]][C], [...R, Result[0][B[0]][C]]>
   : C extends 0 ? [] : [1]
 
-type FullAdd<A extends number[], B extends number[], C extends number = 0> =
-  | A extends [number, ...infer X extends number[]]
+type FullAdd<A extends number[], B extends number[], C extends number = 0, R extends number[] = []> =
+  | R['length'] extends 32
+  ? R
+  : A extends [number, ...infer X extends number[]]
   ? B extends [number, ...infer Y extends number[]]
-  ? [Result[A[0]][B[0]][C], ...FullAdd<X, Y, Carry[A[0]][B[0]][C]>]
+  ? FullAdd<X, Y, Carry[A[0]][B[0]][C], [...R, Result[A[0]][B[0]][C]]>
   : HalfAdd<A, C>
   : HalfAdd<B, C>
 
-export type Trim<A extends number[]> =
-  | A['length'] extends 33
-  ? A extends [...infer X extends number[], number] ? X : A
-  : A
-
-export type Plus<X extends string | number, Y extends string | number> = ToDecimal<Trim<FullAdd<ToBinary<X>, ToBinary<Y>>>>
+export type Plus<X extends string | number, Y extends string | number> = ToDecimal<FullAdd<ToBinary<X>, ToBinary<Y>>>
